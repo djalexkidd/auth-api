3dd2f121ecfe281d1a69b2df251270192ab10ab6
djalexkidd <alexpyr@outlook.fr>
2022-11-30 15:56:47 +0100

    Yo chuis admin mec

--- front/js/is_connected.js
+++ front/js/is_connected.js
@@ -1,5 +1,6 @@
 const guest_buttons = document.querySelector('.guest');
 const member_buttons = document.querySelector('.member');
+const admin_buttons = document.querySelector('.admin');
 const user_status = document.querySelector('.user-status');
 
 const APICALL = "http://localhost:7000/back/get_myself.php";
@@ -14,6 +15,12 @@ async function isConnected() {
             guest_buttons.classList.add("hidden");
 
             user_status.innerHTML = "Connecté en tant que " + data[0].email;
+        } else if (data[0].rank === "admin") {
+            member_buttons.classList.remove("hidden");
+            guest_buttons.classList.add("hidden");
+            admin_buttons.classList.remove("hidden");
+
+            user_status.innerHTML = "Connecté en tant que " + data[0].email + " (Administrateur)";
         }
 }
 
